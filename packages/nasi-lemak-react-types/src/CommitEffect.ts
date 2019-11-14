022f8223b5ef74ccd69416c9a998a2af7b1acd67
Diao Zheng <diaoz@m1.com.sg>
2019-11-14 15:21:03 +0800

    update commiteffect

--- packages/nasi-lemak-react-types/src/CommitEffect.ts
+++ packages/nasi-lemak-react-types/src/CommitEffect.ts
@@ -9,17 +9,9 @@ import asap from "asap/raw";
 /**
  * Use this to ensure that any effects is executed **after** the current Render
  * cycle.
- *
- * We use `setImmediate` with ASAP as fallback since React Native commits to
- * setImmediate at specific times in the render phase.
- * @see https://github.com/facebook/react-native/issues/1331
  */
-export const CommitEffect =
-  typeof document !== "undefined" ?
-    // web
-    setImmediate ?? asap
-  : navigator?.product === "ReactNative" ?
-    // React Native
+export const CommitEffect: (effect: () => void) => void =
+  typeof setImmediate === "function" ?
     setImmediate
   :
     asap
