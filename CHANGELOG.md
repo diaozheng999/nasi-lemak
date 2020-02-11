6dafa41930126f60dca8e9c6eb0823873a7d520c
Diao Zheng <diaoz@m1.com.sg>
2020-02-11 11:53:11 +0800

    add asDispatch

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,12 +1,13 @@
 # 1.2.0
 
-- Updated `nasi` to `1.2.0`
+- Updated `nasi` to `1.2.2`
 - `nasi-lemak-react-types`
   - Added `ReducableDispatcher`
   - Added `ReducerComponent`
   - Added RxJS utility functions
   - Added `Stable` types to represent referentially-stable functions
   - Fix bug which caused `DispatchComponent` to fire side effects in the next reduce cycle.
+  - Added `Rx.asDispatch` to convert an RxJS observable to Dispatch.
 - `nasi-lemak-implementation`
   - Fix bug with `Theme` where themes which are registered cannot be read.
   - Fix bug with `useAsyncLegacy` where promise gets called constantly every render.
--- packages/nasi-lemak-react-types/src/Rx.ts
+++ packages/nasi-lemak-react-types/src/Rx.ts
@@ -6,12 +6,20 @@
 
 // @barrel export all
 
-import { Types } from "nasi";
+import { assert, Option, Types } from "nasi";
 import {
   Observable,
   ObservableInput,
+  OperatorFunction,
   partition as rxpartition,
+  Subject,
 } from "rxjs";
+import { declareAsStable, Dispatch as StableDispatch } from "./Stable";
+
+export interface IObservableDispatch<TAction> {
+  dispatch: StableDispatch<TAction>;
+  close: () => void;
+}
 
 /**
  * A special case of `partition` in RxJS where the predicate is actually a
@@ -22,4 +30,25 @@ export function partition<U, T extends U>(
   predicate: (value: U) => value is T,
 ): [ Observable<T>, Observable<Types.Not<U, T>> ] {
   return rxpartition<any>(source, predicate);
+}
+
+export function asDispatch<InputAction, OutputAction>(
+  output: StableDispatch<OutputAction>,
+  operator: OperatorFunction<InputAction, OutputAction>,
+): IObservableDispatch<InputAction> {
+
+  const input$ = new Subject<InputAction>();
+
+  input$.pipe(operator).subscribe({
+    next: output,
+  });
+
+  return {
+    close: () => input$.complete(),
+    dispatch: declareAsStable((action) => {
+      assert(Option.isSome, action);
+      input$.next(action);
+    }),
+  };
+
 }
